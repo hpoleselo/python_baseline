0898a4654d4a339b469b2be9ff88ddfb344e1183
Henrique Poleselo <hpoleselo@gmail.com>
2023-02-09 23:15:36 -0300

    Small update.

--- utils/custom_logger.py
+++ utils/custom_logger.py
@@ -3,7 +3,7 @@ import logging
 def setup_custom_logger(logger_name: str) -> logging.Logger:
     logging.basicConfig(level=logging.DEBUG,
                 format='[%(levelname)s] - [%(funcName)s] - [%(filename)s:%(lineno)d] - [%(asctime)s] - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
-    logger = logging.getLogger('daily_api_looger')
+    logger = logging.getLogger(logger_name)
     return logger
 
 def output_logs():
