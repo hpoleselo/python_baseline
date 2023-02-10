b22b39ab18a93789f1b40a74e5f46c03e469b128
Henrique Poleselo <hpoleselo@gmail.com>
2023-02-09 23:16:24 -0300

    Added TODO.

--- utils/custom_logger.py
+++ utils/custom_logger.py
@@ -1,6 +1,7 @@
 import logging
 
-def setup_custom_logger(logger_name: str) -> logging.Logger:
+def setup_custom_logger(logger_name: str, logger_level='DEBUG') -> logging.Logger:
+    # TODO: Create dictionary so that when user passes the wished level, it sets up accordingly
     logging.basicConfig(level=logging.DEBUG,
                 format='[%(levelname)s] - [%(funcName)s] - [%(filename)s:%(lineno)d] - [%(asctime)s] - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
     logger = logging.getLogger(logger_name)
